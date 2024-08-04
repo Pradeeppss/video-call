8e2f23677c26f9f0bf837d61387f821c17e06f6d
pradeep-sirpi <pradeep@sirpi.io>
2024-08-04 23:32:10 +0530

    removed auth0

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { Auth0Provider } from "@auth0/auth0-react";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -12,16 +11,10 @@ const client = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={client}>
     <React.StrictMode>
-      <Auth0Provider
-        domain="dev-j43eildeur8ddi7m.us.auth0.com"
-        clientId="NqM2DOrij0TXNAFh3CRBNLMzREEhwOgp"
-        authorizationParams={{ redirect_uri: window.location.origin }}
-      >
-        <BrowserRouter>
-          <App />
-          <ToastContainer position="bottom-right" autoClose={2000} />
-        </BrowserRouter>
-      </Auth0Provider>
+      <BrowserRouter>
+        <App />
+        <ToastContainer position="bottom-right" autoClose={2000} />
+      </BrowserRouter>
     </React.StrictMode>
   </QueryClientProvider>
 );
